10c84744f2584cd627fa8e6001980ae12823001b
Muhammad Farabi <muhammad.farabi@go-jek.com>
2020-09-15 22:15:03 +0700

    Add axios package

--- src/components/cheat-sheets/api-call/api.js
+++ src/components/cheat-sheets/api-call/api.js
@@ -1,11 +1,13 @@
+import axios from 'axios';
+
 const ROOT_URL = 'https://developers.zomato.com/api/v2.1';
 
 export const getCities = city =>
-  fetch(`${ROOT_URL}/cities?q=${city}`, {
+  axios.get(`${ROOT_URL}/cities?q=${city}`, {
     headers: { 'user-key': process.env.REACT_APP_ZOMATO_API_KEY }
-  }).then(res => res.json());
+  });
 
 export const searchRestaurants = cityEntityId =>
-  fetch(`${ROOT_URL}/search?entity_id=${cityEntityId}&entity_type=city`, {
+  axios.get(`${ROOT_URL}/search?entity_id=${cityEntityId}&entity_type=city`, {
     headers: { 'user-key': process.env.REACT_APP_ZOMATO_API_KEY }
-  }).then(res => res.json());
+  });
